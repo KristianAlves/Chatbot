1f3d76f9306c466133339b5e920369f4ebd093eb
Kristian Alves <kristian.alves@gmail.com>
2018-02-27 20:06:11 -0300

    Fix Search Link

--- app/services/interpret_service.rb
+++ app/services/interpret_service.rb
@@ -1,12 +1,25 @@
 class InterpretService
   def self.call action, params
     case action
-    when "list", "search", "search_by_hashtag", "list_link", "search_link", "search_by_link_hashtag"
+    ## Faq Module 
+    when "list", "search", "search_by_hashtag"
       FaqModule::ListService.new(params, action).call()
     when "create", "create_link"
       FaqModule::CreateService.new(params).call()
     when "remove", "remove_link"
       FaqModule::RemoveService.new(params).call()
+
+
+    ## Link Module
+    when "list_link", "search_link", "search_by_link_hashtag"
+      LinkModule::ListService.new(params, action).call()
+    when "create_link"
+      LinkModule::CreateService.new(params).call()
+    when "remove_link"
+      LinkModule::RemoveService.new(params).call()
+
+
+    ## Help Service
     when "help"
       HelpService.call()
     else
