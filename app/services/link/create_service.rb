c4d21be5048dd59bf6a0b0b060479758051ea44f
Kristian Alves <kristian.alves@gmail.com>
2018-03-01 20:11:29 -0300

    Fix Challenge

--- app/services/link/create_service.rb
+++ app/services/link/create_service.rb
@@ -5,11 +5,17 @@ module LinkModule
       @company = Company.last
       @description = params["description-original"]
       @link = params["link-original"]
+      @hashtags = params["hashtags-link-original"]
     end
 
     def call
+      return 'Hashtag Obrigatória' if @hashtags == nil
       Link.transaction do
         link = Link.create(description: @description, link: @link, company: @company)
+        return "Hashtag Obrigatória" if @hashtags == nil
+        @hashtags.split(/[\s,]+/).each do |hashtag|
+          link.hashtags << Hashtag.create(name: hashtag)
+        end
       end
       "Criado com sucesso"
     end
